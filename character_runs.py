007999da0de9d0be7454e177afc3bc00366bb699
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-09-20 18:25:01 +0900

    down line완성

--- character_runs.py
+++ character_runs.py
@@ -29,13 +29,17 @@ def run_rectangle():
     # bottom line
   #  for x in range(50, 750+1, 10):
   #     render_all(x,90)
+  
     # top line
     #for x in range(750, 50 - 1, -10):
      #  render_all(x,550)
 
-    for y in range(90, 550 + 1, 10):
-        render_all(750, y)
-     
+    #up line
+    #for y in range(90, 550 + 1, 10):
+    #   render_all(750, y)
+
+    for y in range(550, 90 - 1, -10):
+        render_all(50, y)
     pass
 
 while True:
